0e95c992e870b39a4d98d22b151d8abbef0bf866
Satvikjain13 <satvikjain0110@gmail.com>
2025-09-18 15:18:52 +0530

    Project

--- src/main/java/com/carrental/DbConnect.java
+++ src/main/java/com/carrental/DbConnect.java
@@ -12,7 +12,7 @@ public class DbConnect {
 	  {
 		   try{
 			   Class.forName("com.mysql.cj.jdbc.Driver");
-			   con = DriverManager.getConnection("jdbc:mysql://localhost:3306/carrentaljsp","root","");
+			   con = DriverManager.getConnection("jdbc:mysql://localhost:3306/carrentaljsp","root","Satvik");
 		   }
 		   catch(Exception e)
 		   {
